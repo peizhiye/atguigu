bb70b894e3f53019dda68dd0f3dc8f37764248f6
peizhiye <598869974@qq.com>
2020-10-17 08:51:18 +0800

    tt

--- java/JUC/juc-day01/src/main/java/com/atguigu/juc/TestCopyOnWriteArrayList.java
+++ java/JUC/juc-day01/src/main/java/com/atguigu/juc/TestCopyOnWriteArrayList.java
@@ -12,45 +12,48 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class TestCopyOnWriteArrayList {
 
-	public static void main(String[] args) {
-		HelloThread ht = new HelloThread();
-		
-		for (int i = 0; i < 10; i++) {
-			new Thread(ht).start();
-		}
-	}
-	
+    public static void main(String[] args) {
+        HelloThread ht = new HelloThread();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(ht).start();
+        }
+    }
+
 }
 
 class HelloThread implements Runnable{
 
-//	private static List<String> list = new ArrayList<String>();	// 异常 java.util.ConcurrentModificationException
+//    private static List<String> list = new ArrayList<String>();	// 异常 java.util.ConcurrentModificationException
 //	private static List<String> list = Collections.synchronizedList(new ArrayList<String>());	// 异常 java.util.ConcurrentModificationException
 
 	private static CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
-	
-	static{
-		list.add("AA");
-		list.add("BB");
-		list.add("CC");
-	}
 
-	@Override
-	public void run() {
-		
-		Iterator<String> it = list.iterator();
+    static{
+        list.add("AA");
+        list.add("BB");
+        list.add("CC");
+    }
 
-		/*	注意：无论是否使用 Collections.synchronizedList 对 List 进行同步化加工，
-				下面这个循环的结果都会抛出 java.util.ConcurrentModificationException 异常，
-				因为都是对同一个 List 进行边迭代、边添加（或删除）的操作。
-				而 CopyOnWriteArrayList 是采用定时复制的原理，操作的并不是同一个 List。
-		 */
-		while(it.hasNext()){
-			System.out.println(it.next());
-			
-			list.add("AA");
-		}
-		
-	}
-	
+    @Override
+    public void run() {
+
+        Iterator<String> it = list.iterator();
+
+        /*	注意：无论是否使用 Collections.synchronizedList 对 List 进行同步化加工，
+                下面这个循环的结果都会抛出 java.util.ConcurrentModificationException 异常，
+                因为都是对同一个 List 进行边迭代、边添加（或删除）的操作。
+                java.util.ArrayList$Itr.next -> java.util.ArrayList$Itr.checkForComodification
+                    if (modCount != expectedModCount)
+                        throw new ConcurrentModificationException();
+                而 CopyOnWriteArrayList 是采用写时复制的原理，操作的并不是同一个 List。
+         */
+        while(it.hasNext()){
+            System.out.println(it.next());
+
+            list.add("AA");
+        }
+
+    }
+
 }
